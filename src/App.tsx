49c59b70f3a65c0aace328fb52509a97f7eb88c7
hennamanao <93893860+hennamanao@users.noreply.github.com>
2022-04-23 18:22:12 +0900

    バグの例

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import axios from "axios";
+import { useState } from "react";
 import "./styles.css";
+import { Todo } from "./Todo";
 
 export default function App() {
+  const [todos, setTodos] = useState<any>([]);
+
   const onClickFetchData = () => {
     axios
       .get("https://jsonplaceholder.typicode.com/todos")
       .then((res) => {
-        console.log(res);
+        setTodos(res.data);
       })
-      .catch(() => {});
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userid} />
+      ))}
     </div>
   );
 }
