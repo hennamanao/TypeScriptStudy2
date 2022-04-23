143c377bc12363b4c098390b971e8356c504bbd5
hennamanao <93893860+hennamanao@users.noreply.github.com>
2022-04-23 18:28:34 +0900

    APIで取得したデータに型を定義する

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import { useState } from "react";
 import "./styles.css";
 import { Todo } from "./Todo";
 
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 export default function App() {
-  const [todos, setTodos] = useState<any>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
 
   const onClickFetchData = () => {
     axios
-      .get("https://jsonplaceholder.typicode.com/todos")
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
       .then((res) => {
         setTodos(res.data);
       })
@@ -20,7 +27,7 @@ export default function App() {
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userid={todo.userid} />
+        <Todo title={todo.title} userid={todo.userId} />
       ))}
     </div>
   );
