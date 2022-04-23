bc164bfe4ad155770dfe2f8a6b17a25f2d3da6ab
hennamanao <93893860+hennamanao@users.noreply.github.com>
2022-04-23 18:48:18 +0900

    propsに型を定義する

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,12 @@ export default function App() {
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userid={todo.userId} />
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
       ))}
     </div>
   );
